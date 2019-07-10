4b6f9068ec87fdebeba7745e05066dd09bd3ae91
Lorena Guido <loremma23@gmail.com>
2019-07-10 14:07:13 -0500

    subiendo el formulario de los heroes permite crear un heroe obtiene la informacion de firebase la renderiza correctamente en una tabla, permite eliminar elementos y la parte de actualizar los elementos tiene un bug que no he logrado encontrar que cambia el objeto pero no lo manda a firebase nuevamente.

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes : HeroeModel[] =[];
+  loading = false;
+
+  constructor( private heroesService:HeroesService) { }
 
   ngOnInit() {
+
+    this.loading = true;
+    this.heroesService.getHeroes()
+        .subscribe( resp => {
+          this.heroes  = resp;
+          this.loading = false;
+        })
   }
+
+
+deleteHeroe( heroe : HeroeModel , i : number){
+
+Swal.fire({
+  title:'¿Estás seguro?',
+  text:`¿Estás seguro que deseas borrar a ${heroe.nombre}`,
+  type: 'question',
+  showConfirmButton: true,
+  showCancelButton: true
+}).then ( resp => {
+
+  if( resp.value === true){
+    this.heroes.splice(i, 1);
+    this.heroesService.deleteHeroe(heroe.id).subscribe();
+
+  }
+})
+
+}
 
 }
